169d82c26e2406c4df0be0586a22aaa6ecbf0f45
Nels Lind <nelslind@gmail.com>
2025-01-23 17:27:40 -0500

    Continuing gravity, class 1/23/25. Added Statistics to environment.

--- gravity.jl
+++ gravity.jl
@@ -8,7 +8,7 @@
 
 cd("/Users/nelslind/Dropbox/teaching/emory/2024-2025/Econ 731 Spring 2025/code/econ731spring25/")
 using Pkg; Pkg.instantiate()
-using FileIO, DataFrames, FixedEffectModels, RegressionTables, Chain, Plots
+using FileIO, DataFrames, FixedEffectModels, RegressionTables, Chain, Plots, Statistics
 
 # location of data files on your computer
 datadir = "/Users/nelslind/Dropbox/data/"
@@ -42,11 +42,59 @@ rename!(aggdf,:rgdpe => :Yot,:pop => :Not)
 aggdf = leftjoin(aggdf,rename(pwt[:,vars],:countrycode => :d,:year => :t),on=[:d,:t])
 rename!(aggdf,:rgdpe => :Ydt,:pop => :Ndt)
 
+# USA imports in 2005
+sample = @chain begin
+    filter(:t => ==(2005),aggdf)
+    filter(:d => ==("USA"),_)
+end
+x = log.(sample.Yot) .- mean(skipmissing(log.(sample.Yot)))
+y = log.(sample.value) .- mean(skipmissing(log.(sample.value)))
+plt = scatter(x,y,legend=false)
+plot!(plt,x,x)
+
+# USA exports in 2005
+sample = @chain begin
+    filter(:t => ==(2005),aggdf)
+    filter(:o => ==("USA"),_)
+end
+x = log.(sample.Ydt) .- mean(skipmissing(log.(sample.Ydt)))
+y = log.(sample.value) .- mean(skipmissing(log.(sample.value)))
+plt = scatter(x,y,legend=false)
+plot!(plt,x,x)
+
+# merge in CEPII
+println(names(dist))
+aggdf = leftjoin(aggdf,rename(dist[:,[:iso_o,:iso_d,:contig,:comlang_off,:comlang_ethno,:distw]],:iso_o => :o,:iso_d => :d),on=[:o,:d])
+
+# USA exports in 2005
+sample = @chain begin
+    filter(:t => ==(2005),aggdf)
+    filter(:o => ==("USA"),_)
+end
+plt = scatter(log.(sample.distw),log.(sample.value ./ sample.Ydt),legend=false)
+x = log.(sample.distw) .- mean(skipmissing(log.(sample.distw)))
+y = log.(sample.value ./ sample.Ydt) .- mean(skipmissing(log.(sample.value ./ sample.Ydt)))
+plt = scatter(x,y,legend=false)
+plot!(plt,x,-x)
+
 # USA exports in 2005
 sample = @chain begin
     filter(:t => ==(2005),aggdf)
     filter(:d => ==("USA"),_)
 end
-plt = scatter(log.(sample.Yot),log.(sample.value),legend=false)
-plot!(plt,log.(sample.Yot),log.(sample.Yot))
+plt = scatter(log.(sample.distw),log.(sample.value ./ sample.Yot),legend=false)
+x = log.(sample.distw) .- mean(skipmissing(log.(sample.distw)))
+y = log.(sample.value ./ sample.Yot) .- mean(skipmissing(log.(sample.value ./ sample.Yot)))
+plt = scatter(x,y,legend=false)
+plot!(plt,x,-x)
 
+# classic gravity regression
+vcov = Vcov.cluster(:o,:d)
+f = @formula log(value) ~ log(Yot) + log(Ydt) + log(distw)
+
+# gravity regression consistent with CES model, use FE's to control for market access and multilateral resistance
+result1 = reg(aggdf,f,vcov)
+f = @formula log(value) ~ fe(o)&fe(t) + fe(d)&fe(t) + log(distw)
+result2 = reg(aggdf,f,vcov)
+
+regtable(result1,result2)
