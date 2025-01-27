528e5e03d1145f601cc7ce7a0c3b5522586671b4
Nels Lind <nelslind@gmail.com>
2025-01-27 14:55:02 -0500

    Updated tradeDataSITC.jl to also aggregate data to SITC 2 digit level

--- tradeDataSITC.jl
+++ tradeDataSITC.jl
@@ -2,7 +2,7 @@
 #   Creates a dataset on bilateral trade flows and tariffs. Uses HS92 level
 #       trade flow data in the BACI data set from CEPII and SITC revision 2
 #       level tariff data from WTD. Additionally, need a concordance between
-#       HS92 and SITC2, which comes from WITS. All these raw data soucres
+#       HS92 and SITC2, which comes from WITS. All these raw data sources
 #       should be in the "datadir."
 #
 #   Raw data sources:
@@ -10,9 +10,10 @@
 #       2. WTD: https://www.dropbox.com/sh/2qwxckjxzj7zdek/AACo4xeB__rwYpRPOi-CUo4ha?dl=0
 #       3. HS0 to SITC2 Concordance: https://wits.worldbank.org/data/public/concordance/Concordance_H0_to_S2.zip
 #
-#   Creates two files in "datadir":
+#   Creates three files in "datadir":
 #       1. datadir*"tradedata/countryNames.csv"
 #       2. datadir*"tradedata/tradeDataSITC.csv"
+#       3. datadir*"tradedata/tradeDataSITC2Digit.csv"
 #
 #   Nels Lind, 1/21/2025
 #
@@ -105,3 +106,40 @@ countries = unique(vcat(rename(df[:,[:o,:exporter]],:o=>:n,:exporter=>:country),
 
 save(dir*"countryNames.csv",countries)
 save(dir*"tradeDataSITC.csv",df[:,Not([:importer,:exporter])])
+
+# aggregate to two digit level
+
+# merge in first year values
+df = @chain begin
+    filter(:t => ==(1995),df)
+    select([:s,:o,:d,:value])
+    rename(_,:value => :value95)
+    leftjoin(df,_,on=[:s,:o,:d])
+end
+
+# reduce to two digit codes
+df.s = map(x->x[1:2],df.s)
+
+# aggregate by summing value, and taking the mean of tariffs with various weights
+function weightedMean(w,x) 
+    if all(ismissing.(w .* x))
+        return missing
+    else
+        return sum(skipmissing(w .* x)) / sum(skipmissing(w .* ismissing.(x)))
+    end
+end
+df = @chain begin
+    df
+    groupby([:s,:o,:d,:t,:exporter,:importer])
+    combine(
+        :value => sum => :value,
+        :t1 => (x->mean(skipmissing(x))) => :t1_mean,
+        [:value,:t1] => weightedMean => :t1_wmean,
+        [:value95,:t1] => weightedMean => :t1_wmean95,
+        :t1_pref => (x->mean(skipmissing(x))) => :t1_pref_mean,
+        [:value,:t1_pref] => weightedMean => :t1_pref_wmean,
+        [:value95,:t1_pref] => weightedMean => :t1_pref_wmean96
+    )
+end
+
+save(dir*"tradeDataSITC2Digit.csv",df[:,Not([:importer,:exporter])])
